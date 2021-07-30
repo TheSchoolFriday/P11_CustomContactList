8f4565f722229806ab50a49f9d0e80026d572ab8
TheSchoolFriday <20046797@myrp.edu.sg>
2021-07-30 10:05:18 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20046797/democustomcontactlist/Contact.java
@@ -0,0 +1,50 @@
+package sg.edu.rp.c346.id20046797.democustomcontactlist;
+
+public class Contact {
+
+    private String name;
+    private String countryCode;
+    private String phoneNum;
+    private char gender;
+
+    public Contact(String name, String countryCode, String phoneNum, char gender) {
+        this.name = name;
+        this.countryCode = countryCode;
+        this.phoneNum = phoneNum;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+
+}
